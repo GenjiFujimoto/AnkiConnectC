7979899f9275fdd51e34b9a539b9ac4df03d959f
GenjiFujimoto <dadra102@hhu.de>
2023-12-22 13:39:03 +0100

    Initial commit

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gcc `pkg-config --cflags glib-2.0` -shared -o libankiconnect.so ankiconnect.c `pkg-config --libs glib-2.0` -fPIC || exit
+gcc `pkg-config --cflags glib-2.0` -shared -o libankiconnectc.so ankiconnectc.c `pkg-config --libs glib-2.0` -fPIC || exit
 
-sudo cp ./ankiconnect.h /usr/local/include/
-sudo cp ./libankiconnect.so /usr/lib
+sudo cp ./ankiconnectc.h /usr/local/include/
+sudo cp ./libankiconnectc.so /usr/lib
