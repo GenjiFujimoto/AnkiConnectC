d4475d71aea94e2c4fa90cba5f17d57b4e93c9c8
GenjiFujimoto <dadra102@hhu.de>
2023-12-25 10:07:20 +0100

    Remove install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-gcc `pkg-config --cflags glib-2.0` -shared -o libankiconnectc.so ankiconnectc.c `pkg-config --libs glib-2.0` -fPIC || exit
-
-sudo cp ./ankiconnectc.h /usr/local/include/
-sudo cp ./libankiconnectc.so /usr/lib
